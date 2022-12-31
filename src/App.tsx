837016ecc9fd0489fb46f9e9a7e52e3ab5ce000b
tmakic <tomakichouen@gmail.com>
2022-12-31 14:00:53 +0900

    天気取得APIを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axiosBase from "axios";
+
+const axios = axiosBase.create({
+  baseURL: process.env.REACT_APP_WEATHER_API_BASE_URL
+});
 
 function App() {
+  axios.get("/onecall", {
+    params: {
+      lat: 35.681236,
+      lon: 139.767125,
+      units: "metric",
+      lang: "jp",
+      appid:process.env.REACT_APP_WEATHER_API_KEY
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
